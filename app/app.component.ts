621109b0d6deb89bbf598b3654047945f17e4aef
DRypa <dr.drypa.k@mail.ru>
2017-01-05 13:41:10 +0300

    move heroes logic to another component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,32 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import Hero from "./Hero";
-import HeroService from "./hero.service";
 
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html',
-  styleUrls: ['app/app.component.css'],
-  providers: [HeroService]
+  templateUrl: 'app/app.component.html'
 })
 export class AppComponent implements OnInit {
-  heroes:Array<Hero>;
 
-  constructor(private heroService:HeroService) {
-  }
-
-  name = 'Angular';
   title = 'Tour of Heroes';
-  selectedHero:Hero;
-  onSelect = (hero:Hero)=> {
-    this.selectedHero = hero;
-    console.log(hero.name);
-  };
 
   ngOnInit():void {
-    this.heroService.getHeroesSlow().then((heroes)=> {
-      this.heroes = heroes;
-      this.selectedHero = this.heroes[0];
-    });
   }
 }
---
+++ app/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import Hero from "./Hero";
+import HeroService from "./hero.service";
+
+@Component({
+  selector: 'heroes',
+  templateUrl: 'app/heroes.component.html',
+  styleUrls: ['app/heroes.component.css'],
+  providers: [HeroService]
+})
+export default class HeroesComponent implements OnInit {
+  constructor(private heroService:HeroService) {
+  }
+
+  heroes:Array<Hero>;
+  selectedHero:Hero;
+  onSelect = (hero:Hero)=> {
+    this.selectedHero = hero;
+    console.log(hero.name);
+  };
+
+  ngOnInit():void {
+    this.heroService.getHeroesSlow().then((heroes)=> {
+      this.heroes = heroes;
+      this.selectedHero = this.heroes[0];
+    });
+  }
+}
