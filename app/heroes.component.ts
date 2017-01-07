e4737bc031d76edcba93791bcbf760bc4ce3937c
DRypa <dr.drypa.k@mail.ru>
2017-01-07 12:24:59 +0300

    small details

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import Hero from "./Hero";
 import HeroService from "./hero.service";
 
@@ -9,15 +10,19 @@ import HeroService from "./hero.service";
   providers: [HeroService]
 })
 export default class HeroesComponent implements OnInit {
-  constructor(private heroService:HeroService) {
+  constructor(private heroService:HeroService, private router:Router) {
   }
 
   heroes:Array<Hero> = [];
   selectedHero:Hero;
-  onSelect = (hero:Hero)=> {
+  onSelect(hero:Hero):void {
     this.selectedHero = hero;
     console.log(hero.name);
-  };
+  }
+
+  gotoDetail():void{
+    this.router.navigate(['/detail', this.selectedHero.id])
+  }
 
   ngOnInit():void {
     this.heroService.getHeroes().then((heroes)=> {
